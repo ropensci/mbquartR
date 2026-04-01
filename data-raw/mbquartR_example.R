47a80936a5e6e419e48601747f48c8ccd647a40a
alex-koiter <akoiter@gmail.com>
2026-04-01 15:14:05 -0500

    added to example again

--- data-raw/mbquartR_example.R
+++ data-raw/mbquartR_example.R
@@ -6,6 +6,7 @@ mbquartR_example <- cache_load() |>
                     "NE-01-012-12E1", "SW-14-008-02E1", "NE-22-008-01E1",
                     "NW-10-009-01E1", "RL-0022-St. Norbert", "NW-29-004-07W1",
                     "SE-02-005-07W1", "SE-07-005-06W1", "SE-08-034-17E1",
-                    "SW-06-035-29W1", "NE-02-012-12E1", "PL-000R-St. Andrews"))
+                    "SW-06-035-29W1", "NE-02-012-12E1", "PL-000R-St. Andrews",
+                    "NW-07-033-29W1"))
 
 usethis::use_data(mbquartR_example, overwrite = TRUE)
